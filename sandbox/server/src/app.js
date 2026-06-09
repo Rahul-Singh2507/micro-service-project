af4e3bae4ae9025cab883aa0612373479ab86fb2
Rahul <rahulsinghraj2000@gmail.com>
2026-06-09 13:01:45 +0530

    pod creation sucessfully

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,7 +1,10 @@
 import express from "express"
 
 import morgan from"morgan"
+import { createPod} from "./kubernetes/pod.js"
+import { createService } from "./kubernetes/service.js"  
 
+import { v7 as uuid } from "uuid"
 const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
@@ -9,6 +12,17 @@ app.use(express.urlencoded({extended:true}))
 app.get("/api/sandbox/health",(req,res)=>{
     res.status(200).json({message:"Sandbox service is healthy"}     )
 })
+app.post("/api/sandbox/create",async (req,res)=>{
+ const sandboxId = uuid()
+await Promise.all([createPod(sandboxId),createService(sandboxId)])
+res.status(201).json({
     
+    message:"Sandbox created successfully",
+    sandboxId,
+    previewUrl:`http://${sandboxId}.preview.localhost`
+
+
+})            
     
+})
 export default app
